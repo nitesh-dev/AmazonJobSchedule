d9949cd87b61af6dc571ee0cb5d9cc54f5d3a665
nitesh-kr <niteshdev547@gmail.com>
2025-06-13 07:48:57 +0530

    electron

---
+++ electron/index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron/main');
+const { session } = require('electron');
+
+const createWindow = async () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  await win.loadURL('https://hiring.amazon.ca/');
+  session.defaultSession
+    .loadExtension(
+      '/home/niteshkr/Documents/Codes/others/AmazonJobSchedule/build'
+    )
+    .then(({ id }) => {
+      console.log(`Extension loaded with ID: ${id}`);
+      // After loading the extension, open the popup.html directly
+      const extensionId = id; // Replace with actual ID from console log
+      const popupUrl = `chrome-extension://${extensionId}/popup.html`;
+      const popupWin = new BrowserWindow({ width: 400, height: 600 });
+      popupWin.loadURL(popupUrl);
+    });
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});

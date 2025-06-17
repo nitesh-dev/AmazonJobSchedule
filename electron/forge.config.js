ba47e7dcfbf6aba5d5268ae9da22fa7cf0a4e321
nitesh-kr <niteshdev547@gmail.com>
2025-06-17 12:48:11 +0530

    refactor(electron): comment out unused maker configuration for squirrel

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -23,10 +23,10 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {},
+    // },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['win32','darwin'],
